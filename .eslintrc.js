5a983d7dbe3fb1bfcaf526e0c1e4f5f4da5c2c52
Ricardo Souza <ricardosouza_defn@outlook.com>
2021-08-23 21:19:28 -0300

    Babel / Eslint / Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ["standard", "prettier"],
+  parserOptions: "babel-eslint",
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
